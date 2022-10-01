1f1ad2aaecc071632cceb238a290e07ee794c94e
be1ieve <will.xd@gmail.com>
2022-10-01 11:48:32 +0800

    Remove deadzone setting

--- v7rc.ino
+++ v7rc.ino
@@ -5,6 +5,7 @@
 
 #include "BLEDevice.h"
 
+#define BLE_DEVICE_NAME        "meArm for V7RC"
 #define UART_SERVICE_UUID      "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
@@ -17,29 +18,29 @@ BLEAdvertData scndata;
 
 #include <AmebaServo.h>
 
-#define FRONT_SERVO_PIN PA13
-#define FRONT_SERVO_MIN 1200
-#define FRONT_SERVO_MAX 1800
-#define FRONT_SERVO_CENTER 1500
-#define FRONT_SERVO_DEADZONE 50
+#define UPWARD_SERVO_PIN PA13
+#define UPWARD_SERVO_MIN 1000
+#define UPWARD_SERVO_MAX 1800
+#define UPWARD_SERVO_START 1200
 
-#define REAR_SERVO_PIN PA12
-#define REAR_SERVO_MIN 1200
-#define REAR_SERVO_MAX 1800
-#define REAR_SERVO_CENTER 1500
-#define REAR_SERVO_DEADZONE 50
+
+#define FORWARD_SERVO_PIN PA26
+#define FORWARD_SERVO_MIN 1200
+#define FORWARD_SERVO_MAX 1900
+#define FORWARD_SERVO_START 1800
+
 
 #define ROTATE_SERVO_PIN PA25
-#define ROTATE_SERVO_MIN 1200
-#define ROTATE_SERVO_MAX 1800
-#define ROTATE_SERVO_CENTER 1500
-#define ROTATE_SERVO_DEADZONE 50
+#define ROTATE_SERVO_MIN 1050
+#define ROTATE_SERVO_MAX 1950
+#define ROTATE_SERVO_START 1500
 
-#define CLAMP_SERVO_PIN PA26
+
+#define CLAMP_SERVO_PIN PA12
 #define CLAMP_SERVO_MIN 1200
-#define CLAMP_SERVO_MAX 1800
-#define CLAMP_SERVO_CENTER 1500
-#define CLAMP_SERVO_DEADZONE 50
+#define CLAMP_SERVO_MAX 1600
+#define CLAMP_SERVO_START 1500
+
 
 AmebaServo frontArmServo, rearArmServo, rotateServo, clampServo;
 
@@ -57,11 +58,10 @@ void setup() {
     Serial.begin(115200);
 
     advdata.addFlags(GAP_ADTYPE_FLAGS_LIMITED | GAP_ADTYPE_FLAGS_BREDR_NOT_SUPPORTED);
-    advdata.addCompleteName("AMEBA_BLE_DEV");
+    advdata.addCompleteName(BLE_DEVICE_NAME);
     scndata.addCompleteServices(BLEUUID(UART_SERVICE_UUID));
 
-    Rx.setWriteProperty(true);
-    Rx.setWriteNRProperty(true);
+    Rx.setWriteNRProperty(true);  // Enable write without response (write command)
     Rx.setWritePermissions(GATT_PERM_WRITE);
     Rx.setWriteCallback(writeCB);
     Rx.setBufferLen(STRING_BUF_SIZE);
@@ -76,8 +76,8 @@ void setup() {
 
     BLE.beginPeripheral();
 
-    frontArmServo.attach(FRONT_SERVO_PIN);
-    rearArmServo.attach(REAR_SERVO_PIN);
+    frontArmServo.attach(UPWARD_SERVO_PIN);
+    rearArmServo.attach(FORWARD_SERVO_PIN);
     rotateServo.attach(ROTATE_SERVO_PIN);
     clampServo.attach(CLAMP_SERVO_PIN);
 
@@ -91,10 +91,10 @@ void loop() {
       // CH2: rear arm servo
       // CH3: rotate servo
       // CH4: clamp servo
-      int ch1=FRONT_SERVO_CENTER, ch2=REAR_SERVO_CENTER, ch3=ROTATE_SERVO_CENTER, ch4=CLAMP_SERVO_CENTER;
+      int ch1=UPWARD_SERVO_START, ch2=FORWARD_SERVO_START, ch3=ROTATE_SERVO_START, ch4=CLAMP_SERVO_START;
       char charBuff[3]; // for string to int function
 
-      //Serial.println(rxString); // raw output
+      Serial.println(rxString); // raw output
       if((rxString.substring(0,3).compareTo("SRV") == 0) || (rxString.substring(0,3).compareTo("SRT") == 0)){
         ch1 = rxString.substring(3,7).toInt();
         ch2 = rxString.substring(7,11).toInt();
@@ -113,29 +113,10 @@ void loop() {
       }
 
       /* Process servo data */
-      if(ch1 < 1500 - FRONT_SERVO_DEADZONE)
-        ch1 = map(ch1, 1000, 1500 - FRONT_SERVO_DEADZONE, FRONT_SERVO_MIN, FRONT_SERVO_CENTER);
-      else if (ch1 > 1500 + FRONT_SERVO_DEADZONE)
-        ch1 = map(ch1, 1500 + FRONT_SERVO_DEADZONE, 2000, FRONT_SERVO_CENTER, FRONT_SERVO_MAX);
-      else ch1 = FRONT_SERVO_CENTER;
-
-      if(ch2 < 1500 - REAR_SERVO_DEADZONE)
-        ch2 = map(ch2, 1000, 1500 - REAR_SERVO_DEADZONE, REAR_SERVO_MIN, REAR_SERVO_CENTER);
-      else if (ch2 > 1500 + REAR_SERVO_DEADZONE)
-        ch2 = map(ch2, 1500 + REAR_SERVO_DEADZONE, 2000, REAR_SERVO_CENTER, REAR_SERVO_MAX);
-      else ch2 = REAR_SERVO_CENTER;
-
-      if(ch3 < 1500 - ROTATE_SERVO_DEADZONE)
-        ch3 = map(ch3, 1000, 1500 - ROTATE_SERVO_DEADZONE, ROTATE_SERVO_MIN, ROTATE_SERVO_CENTER);
-      else if (ch3 > 1500 + ROTATE_SERVO_DEADZONE)
-        ch3 = map(ch3, 1500 + ROTATE_SERVO_DEADZONE, 2000, ROTATE_SERVO_CENTER, ROTATE_SERVO_MAX);
-      else ch3 = ROTATE_SERVO_CENTER;
-
-      if(ch4 < 1500 - CLAMP_SERVO_DEADZONE)
-        ch4 = map(ch4, 1000, 1500 - CLAMP_SERVO_DEADZONE, CLAMP_SERVO_MIN, CLAMP_SERVO_CENTER);
-      else if (ch4 > 1500 + CLAMP_SERVO_DEADZONE)
-        ch4 = map(ch4, 1500 + CLAMP_SERVO_DEADZONE, 2000, CLAMP_SERVO_CENTER, CLAMP_SERVO_MAX);
-      else ch4 = CLAMP_SERVO_CENTER;
+      ch1 = map(ch1, 1000, 2000, UPWARD_SERVO_MIN, UPWARD_SERVO_MAX);
+      ch2 = map(ch2, 1000, 2000, FORWARD_SERVO_MIN, FORWARD_SERVO_MAX);
+      ch3 = map(ch3, 1000, 2000, ROTATE_SERVO_MIN, ROTATE_SERVO_MAX);
+      ch4 = map(ch4, 1000, 2000, CLAMP_SERVO_MIN, CLAMP_SERVO_MAX);
 
       /* Print out servo data */
 
